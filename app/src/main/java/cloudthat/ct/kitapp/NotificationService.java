7e8a5470f35da0ef40e11a87e314e6a12977dfbe
Adit <blackice9107@gmail.com>
2017-11-27 11:38:40 +0530

    Change icons

--- app/src/main/java/cloudthat/ct/kitapp/NotificationService.java
+++ app/src/main/java/cloudthat/ct/kitapp/NotificationService.java
@@ -1,8 +1,25 @@
 package cloudthat.ct.kitapp;
 
+import android.app.Notification;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
 /**
  * Created by Adit on 11/24/2017.
  */
 
-public class NotificationService {
+public class NotificationService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Notification notification = new Notification.Builder(this)
+                .setContentTitle(remoteMessage.getNotification().getTitle())
+                .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                .build();
+        NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
+        manager.notify(123, notification);
+    }
 }
